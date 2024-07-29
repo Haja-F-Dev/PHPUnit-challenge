2beeb5ee79e13a5b271132ae9e5d15cbb274015f
Haja-F-Dev <frhajadev@gmail.com>
2024-07-29 10:47:11 +0200

    Creation des méthodes dans les classes demandées

--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -22,4 +22,38 @@ class CalculatorTest extends TestCase
             "La méthode sum() ne fonctionne plus :-("
         );
     }
+
+    public function testMultiplication(): void
+    {
+        //Arrange
+        $firstValue = 5;
+        $secondValue = 8;
+        $expectedResult = 40;
+
+        //Act
+        $calculator = new Calculator();
+        $multiplication = $calculator->multiplication($firstValue, $secondValue);
+        
+        //Assert
+        $this->assertSame($expectedResult, $multiplication, 
+            "La méthode multiplication() ne fonctionne plus :-("
+        );
+    }
+
+    public function testIsEven(): void
+    {
+        //Arrange
+        $value = 5;
+        $expectedResult = false;
+
+        //Act
+        $calculator = new Calculator();
+        $isEven = $calculator->isEven($value);
+        
+        //Assert
+        // Test pour un nombre impair
+        $this->assertFalse($isEven, $expectedResult, 
+            "Le nombre $value devrait être impair"
+        );
+    }
 }
